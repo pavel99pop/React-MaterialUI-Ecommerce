5a2b68a61279944374f43d45270c92d0e70b27a6
pavel99pop <pavel99pop@gmail.com>
2021-07-02 15:47:15 +0600

    Adding products to cart and dynamic cart icon on navbar

--- react-ecommerce/src/components/Navbar/Navbar.jsx
+++ react-ecommerce/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ import { ShoppingCart } from "@material-ui/icons";
 import logo from "../../assets/nav-logo.png";
 import useStyles from "./styles";
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
 
   return (
@@ -31,7 +31,7 @@ const Navbar = () => {
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton aria-label="Show cart Items" color="inherit">
-              <Badge badgeContent={2} color="secondary">
+              <Badge badgeContent={totalItems} color="secondary">
                 <ShoppingCart />
               </Badge>
             </IconButton>
