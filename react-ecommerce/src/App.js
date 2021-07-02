ce67ec9ccf3e08e2a7c7b85ec589f915331e4691
pavel99pop <pavel99pop@gmail.com>
2021-07-02 12:40:39 +0600

    Navbar component and styling

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   );
